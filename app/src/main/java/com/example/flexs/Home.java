f8a5f41a55e885a7e4bfadfc778d0052b778f2ab
rifkyandry <rifkyandryanto7@gmail.com>
2025-01-13 22:51:35 +0700

    update horizontal to vertical

--- app/src/main/java/com/example/flexs/Home.java
+++ app/src/main/java/com/example/flexs/Home.java
@@ -32,20 +32,31 @@ public class Home extends AppCompatActivity {
         recyclerView = findViewById(R.id.reciclerView1);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        String getData = getString(R.string.drStrange);
+        String TextDrStrange = getString(R.string.drStrange);
+        String TextMoana = getString(R.string.moana);
+        String TextCaptainAmerica = getString(R.string.CaptenAmerica);
+        String TextCaptainMarvel = getString(R.string.CaptenMarvel);
+        String TextBrave = getString(R.string.Brave);
+        String TextGroot = getString(R.string.groot);
+        String Textfroozen = getString(R.string.froozen);
+        String TextAvangers = getString(R.string.avangers);
+        String TextRaya = getString(R.string.raya);
+        String TextIronMan = getString(R.string.ironman);
+        String TextInside = getString(R.string.inside);
+        String TextToy = getString(R.string.toystory);
 
-        customModelArrayList.add(new CustomModel(R.drawable.drstrange, "Doctor Strange",getData));
-        customModelArrayList.add(new CustomModel(R.drawable.moana, "Moana","Animation, Coming of Age"));
-        customModelArrayList.add(new CustomModel(R.drawable.captenamerica, "Captain America","Action, Adventure"));
-        customModelArrayList.add(new CustomModel(R.drawable.captenmarvel, "Captain Marvel","Action, Adventure"));
-        customModelArrayList.add(new CustomModel(R.drawable.brave, "Brave","Animation, Coming of Age"));
-        customModelArrayList.add(new CustomModel(R.drawable.groot, "I Am Groot","Action, Adventure"));
-        customModelArrayList.add(new CustomModel(R.drawable.frozen2, "Frozen II","Animation, fantasy"));
-        customModelArrayList.add(new CustomModel(R.drawable.avangers, "Avangers","Action, Adventure"));
-        customModelArrayList.add(new CustomModel(R.drawable.raya, "Raya","Mitology"));
-        customModelArrayList.add(new CustomModel(R.drawable.ironman, "Iron Man","Action, Adventure"));
-        customModelArrayList.add(new CustomModel(R.drawable.inside2, "Inside 2","Animation, Magic"));
-        customModelArrayList.add(new CustomModel(R.drawable.toystory, "Toy Storys 4","Animation"));
+        customModelArrayList.add(new CustomModel(R.drawable.drstrange, "DOCTOR STRANGE : MULTIVERSE OF MADNESS",TextDrStrange));
+        customModelArrayList.add(new CustomModel(R.drawable.moana, "MOANA",TextMoana));
+        customModelArrayList.add(new CustomModel(R.drawable.captenamerica, "CAPTAIN AMERICA : THE FIRST AVENGER",TextCaptainAmerica));
+        customModelArrayList.add(new CustomModel(R.drawable.captenmarvel, "CAPTAIN MARVEL",TextCaptainMarvel));
+        customModelArrayList.add(new CustomModel(R.drawable.brave, "BRAVE",TextBrave));
+        customModelArrayList.add(new CustomModel(R.drawable.groot, "I AM GROOT",TextGroot));
+        customModelArrayList.add(new CustomModel(R.drawable.frozen2, "FROOZEN II",Textfroozen));
+        customModelArrayList.add(new CustomModel(R.drawable.avangers, "AVENGERS",TextAvangers));
+        customModelArrayList.add(new CustomModel(R.drawable.raya, "RAYA AND THE LAST DRAGON",TextRaya));
+        customModelArrayList.add(new CustomModel(R.drawable.ironman, "IRON MAN",TextIronMan));
+        customModelArrayList.add(new CustomModel(R.drawable.inside2, "INSIDE OUT 2",TextInside));
+        customModelArrayList.add(new CustomModel(R.drawable.toystory, "TOY STORY 4",TextToy));
 
 
         CustomAdapter customAdapter = new CustomAdapter(this, customModelArrayList);
